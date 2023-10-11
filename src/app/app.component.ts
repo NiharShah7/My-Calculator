131e95fe1cd77df5971013db2c01bae1f85a8842
NiharShah7 <niharshah554@gmail.com>
2023-10-11 13:42:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'MyCalculator';
+
+  inputValue = '0';
+
+  currentValue = '';
+
+  SendValueToInput(value: any) {
+    console.log(value);
+    this.currentValue = this.currentValue + value;
+
+    this.inputValue = this.currentValue;
+    console.log(this.inputValue);
+  }
+
+  Calculate() {
+    this.inputValue = eval(this.currentValue);
+    this.currentValue = this.inputValue;
+  }
+
+  ClearAll(){
+    this.inputValue='0'
+    this.currentValue=''
+
+  }
+
+  Back(){
+    this.currentValue=this.currentValue.slice(0,-1)
+    this.inputValue=this.currentValue;
+  }
 }
